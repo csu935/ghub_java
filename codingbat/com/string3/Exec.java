9016e8092cd67c26b29b29db11bdeef5c69e1952
marsbarmania <reg.marsbarmania@gmail.com>
2013-06-13 14:18:06 +0900

    mirrorEnds

--- codingbat/com/string3/Exec.java
+++ codingbat/com/string3/Exec.java
@@ -11,6 +11,22 @@ public class Exec {
 		Exec my = new Exec();
 	}
 	/*
+	 * Given a string, look for a mirror image (backwards) string 
+	 * at both the beginning and end of the given string. 
+	 * In other words, zero or more characters at the very begining 
+	 * of the given string, and at the very end of the string 
+	 * in reverse order (possibly overlapping). 
+	 * For example, the string "abXYZba" has the mirror end "ab". 
+	 * 
+	 * mirrorEnds("abXYZba") → "ab"
+	 * mirrorEnds("abca") → "a"
+	 * mirrorEnds("aba") → "aba"
+	 */
+	public String mirrorEnds(String string) {
+		return "";
+	}
+	
+	/*
 	 * Given a string, return the longest substring 
 	 * that appears at both the beginning and end of 
 	 * the string without overlapping. For example, 
