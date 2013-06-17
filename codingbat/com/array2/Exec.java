70a5a80e57ccb7a014c2d2fc0bc3154beec02e5a
marsbarmania <reg.marsbarmania@gmail.com>
2013-06-17 16:21:22 +0900

    added array2

---
+++ codingbat/com/array2/Exec.java
@@ -0,0 +1,28 @@
+package codingbat.com.array2;
+import lib.DeOut;
+
+public class Exec {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // Exec my = new Exec();
+  }
+  /*
+   * Return the number of even ints in the given array.
+   * Note: the % "mod" operator computes the remainder,
+   * e.g. 5 % 2 is 1.
+   *
+   * countEvens({2, 1, 2, 3, 4}) → 3
+   * countEvens({2, 2, 0}) → 3
+   * countEvens({1, 3, 5}) → 0
+   */
+  public int countEvens(int[] nums) {
+    int count=0;
+    for(int i=0;i<nums.length;i++){
+      if(nums[i]%2==0) count++;
+    }
+    return count;
+  }
+}
