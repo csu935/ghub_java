310047f287d7ee4507cfb12b88f22b476b437cad
marsbarmania <reg.marsbarmania@gmail.com>
2013-06-17 15:50:45 +0900

    notReplace

--- codingbat/com/string3/Exec.java
+++ codingbat/com/string3/Exec.java
@@ -10,15 +10,61 @@ public class Exec {
 	public static void main(String[] args) {
 		Exec my = new Exec();
 	}
-	
+
 	/*
-	 * Given a string, return the length of the largest "block" 
-	 * in the string. A block is a run of adjacent chars that are the same. 
-	 * 
+	 * Given a string, return a string where every appearance
+	 * of the lowercase word "is" has been replaced with "is not".
+	 * The word "is" should not be immediately preceeded or followed
+	 * by a letter -- so for example the "is" in "this" does not count.
+	 * (Note: Character.isLetter(char) tests if a char is a letter.)
+	 *
+	 * notReplace("is test") → "is not test"
+	 * notReplace("is-is") → "is not-is not"
+	 * notReplace("This is right") → "This is not right"
+	 */
+	public String notReplace(String str) {
+		String answer = "";
+		if(str.length()<2){
+			return str;
+		}else if(str.length()==2){
+			if(str.substring(0,2).equals("is")){
+				return "is not";
+			}else{
+				return str;
+			}
+		}else{
+			for(int i=0;i<str.length();i++){
+				if(i<str.length()-1 && str.substring(i,i+2).equals("is")){
+					if(i==0 && !Character.isLetter(str.charAt(i+2))){
+						answer += "is not";
+						i++;
+
+					}else if(i==str.length()-2 && !Character.isLetter(str.charAt(i-1))){
+						answer += "is not";
+						i++;
+					}else if((i>0 && i+2<str.length()) &&
+							!Character.isLetter(str.charAt(i-1))&&!Character.isLetter(str.charAt(i+2))){
+						answer += "is not";
+						i++;
+					}else{
+						answer += str.substring(i,i+1);
+					}
+				}else{
+					answer += str.substring(i,i+1);
+				}
+
+			}
+			return answer;
+		}
+	}
+	/*
+	 * Given a string, return the length of the largest "block"
+	 * in the string. A block is a run of adjacent chars that are the same.
+	 *
 	 * maxBlock("hoopla") → 2
 	 * maxBlock("abbCCCddBBBxx") → 3
 	 * maxBlock("") → 0
-	 * 
+	 *
 	 * maxBlock("xyz") → 1
 	 * maxBlock("xxyz") → 2
 	 * maxBlock("xyzz") → 2
@@ -26,14 +72,14 @@ public class Exec {
 	 * maxBlock("XXBBBbbxxXXXX") → 4
 	 */
 	public int maxBlock(String str) {
-		
+
 		if(str.length()==0) return 0;
 		int maxBlockNum = 1;
 		int currentSecNum = 1;
-		
+
 		String currentBlockString= "";
 		for(int i=0;i<str.length();i++){
-			
+
 			if(Character.toString(str.charAt(i)).equals(currentBlockString)){
 				currentSecNum++;
 				maxBlockNum = Math.max(maxBlockNum, currentSecNum);
@@ -43,18 +89,18 @@ public class Exec {
 				currentBlockString = str.substring(i, i+1);
 			}
 		}
-		
+
 		return maxBlockNum;
 	}
-	
+
 	/*
-	 * Given a string, look for a mirror image (backwards) string 
-	 * at both the beginning and end of the given string. 
-	 * In other words, zero or more characters at the very begining 
-	 * of the given string, and at the very end of the string 
-	 * in reverse order (possibly overlapping). 
-	 * For example, the string "abXYZba" has the mirror end "ab". 
-	 * 
+	 * Given a string, look for a mirror image (backwards) string
+	 * at both the beginning and end of the given string.
+	 * In other words, zero or more characters at the very begining
+	 * of the given string, and at the very end of the string
+	 * in reverse order (possibly overlapping).
+	 * For example, the string "abXYZba" has the mirror end "ab".
+	 *
 	 * mirrorEnds("abXYZba") → "ab"
 	 * mirrorEnds("abca") → "a"
 	 * mirrorEnds("aba") → "aba"
@@ -75,13 +121,13 @@ public class Exec {
 		}
 		return toRight;
 	}
-	
+
 	/*
-	 * Given a string, return the longest substring 
-	 * that appears at both the beginning and end of 
-	 * the string without overlapping. For example, 
-	 * sameEnds("abXab") is "ab". 
-	 * 
+	 * Given a string, return the longest substring
+	 * that appears at both the beginning and end of
+	 * the string without overlapping. For example,
+	 * sameEnds("abXab") is "ab".
+	 *
 	 * sameEnds("abXYab") → "ab"
 	 * sameEnds("xx") → "x"
 	 * sameEnds("xxx") → "x"
@@ -90,30 +136,30 @@ public class Exec {
 		int limit = string.length()%2==0? string.length()/2:string.length()/2+1;
 		String front = string.substring(0,string.length()/2);
 		String tail = string.substring(limit);
-		
+
 		if(tail.equals(front)){
 			return front;
 		}else{
 			int i = 1;
 			while(i<string.length()/2){
-				
+
 				if(tail.substring(i).equals(front.substring(0, front.length()-i))){
 					return tail.substring(i);
 				}
 				i++;
 			}
 		}
-		
+
 		return "";
 	}
 	/*
-	 * Given a string, return the sum of the digits 0-9 
-	 * that appear in the string, ignoring all other characters. 
-	 * Return 0 if there are no digits in the string. 
-	 * (Note: Character.isDigit(char) tests 
-	 * if a char is one of the chars '0', '1', .. '9'. 
-	 * Integer.parseInt(string) converts a string to an int.) 
-	 * 
+	 * Given a string, return the sum of the digits 0-9
+	 * that appear in the string, ignoring all other characters.
+	 * Return 0 if there are no digits in the string.
+	 * (Note: Character.isDigit(char) tests
+	 * if a char is one of the chars '0', '1', .. '9'.
+	 * Integer.parseInt(string) converts a string to an int.)
+	 *
 	 * sumDigits("aa1bc2d3") → 6
 	 * sumDigits("aa11b33") → 8
 	 * sumDigits("Chocolate") → 0
@@ -129,10 +175,10 @@ public class Exec {
 		return sum;
 	}
 	/*
-	 * We'll say that a "triple" in a string is a char appearing three times in a row. 
-	 * Return the number of triples in the given string. 
-	 * The triples may overlap. 
-	 * 
+	 * We'll say that a "triple" in a string is a char appearing three times in a row.
+	 * Return the number of triples in the given string.
+	 * The triples may overlap.
+	 *
 	 * countTriple("abcXXXabc") → 1
 	 * countTriple("xxxabyyyycd") → 3
 	 * countTriple("a") → 0
@@ -147,10 +193,10 @@ public class Exec {
 		return count;
 	}
 	/*
-	 * We'll say that a "triple" in a string is a char appearing three times in a row. 
-	 * Return the number of triples in the given string. 
-	 * The triples may overlap. 
-	 * 
+	 * We'll say that a "triple" in a string is a char appearing three times in a row.
+	 * Return the number of triples in the given string.
+	 * The triples may overlap.
+	 *
 	 * countTriple("abcXXXabc") → 1
 	 * countTriple("xxxabyyyycd") → 3
 	 * countTriple("a") → 0
@@ -165,21 +211,21 @@ public class Exec {
 		return count;
 	}
 	/*
-	 * We'll say that a lowercase 'g' in a string is "happy" 
-	 * if there is another 'g' immediately to its left or right. 
-	 * Return true if all the g's in the given string are happy. 
-	 * 
+	 * We'll say that a lowercase 'g' in a string is "happy"
+	 * if there is another 'g' immediately to its left or right.
+	 * Return true if all the g's in the given string are happy.
+	 *
 	 * gHappy("xxggxx") → true
 	 * gHappy("xxgxx") → false
 	 * gHappy("xxggyygxx") → false
-	 * 
+	 *
 	 * gHappy("g") → false
 	 * gHappy("") → true
 	 * gHappy("xxgggxyz") → true
 	 * gHappy("yyygggxyy") → true
 	 */
 	public boolean gHappy(String str) {
-		
+
 		if(str.length()==0) return true;
 		if(str.length()==1) return false;
 		boolean happy = false;
@@ -199,10 +245,10 @@ public class Exec {
 		return happy;
 	}
 	/*
-	 * Given a string, return true if the number of appearances 
-	 * of "is" anywhere in the string is equal to the number 
-	 * of appearances of "not" anywhere in the string (case sensitive). 
-	 * 
+	 * Given a string, return true if the number of appearances
+	 * of "is" anywhere in the string is equal to the number
+	 * of appearances of "not" anywhere in the string (case sensitive).
+	 *
 	 * equalIsNot("This is not") → false
 	 * equalIsNot("This is notnot") → true
 	 * equalIsNot("noisxxnotyynotxisi") → true
@@ -221,42 +267,42 @@ public class Exec {
 		return isCnt==notCnt;
 	}
 	/*
-	 * Given two strings, base and remove, 
-	 * return a version of the base string 
-	 * where all instances of the remove string have been removed (not case sensitive). 
-	 * You may assume that the remove string is length 1 or more. 
-	 * Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x". 
-	 * 
+	 * Given two strings, base and remove,
+	 * return a version of the base string
+	 * where all instances of the remove string have been removed (not case sensitive).
+	 * You may assume that the remove string is length 1 or more.
+	 * Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
+	 *
 	 * withoutString("Hello there", "llo") → "He there"
 	 * withoutString("Hello there", "e") → "Hllo thr"
 	 * withoutString("Hello there", "x") → "Hello there"
-	 * 
+	 *
 	 */
 	public String withoutString(String base, String remove) {
 		String answer = "";
 		int checkMax = base.length()-remove.length()+1;
-		for(int i=0;i<base.length();i++){			
-			if(i<checkMax && base.toLowerCase().substring(i, i+remove.length()).equals(remove.toLowerCase())){				
-				i += remove.length()-1;				
+		for(int i=0;i<base.length();i++){
+			if(i<checkMax && base.toLowerCase().substring(i, i+remove.length()).equals(remove.toLowerCase())){
+				i += remove.length()-1;
 			}else{
 				answer += base.substring(i, i+1);
 			}
 		}
-		
+
 		return answer;
 	}
 	/*
-	 * Given a string, count the number of words ending in 'y' 
-	 * or 'z' -- so the 'y' in "heavy" and the 'z' in "fez" count, 
-	 * but not the 'y' in "yellow" (not case sensitive). 
-	 * We'll say that a y or z is at the end of a word 
-	 * if there is not an alphabetic letter immediately following it. 
-	 * (Note: Character.isLetter(char) tests if a char is an alphabetic letter.) 
-	 * 
+	 * Given a string, count the number of words ending in 'y'
+	 * or 'z' -- so the 'y' in "heavy" and the 'z' in "fez" count,
+	 * but not the 'y' in "yellow" (not case sensitive).
+	 * We'll say that a y or z is at the end of a word
+	 * if there is not an alphabetic letter immediately following it.
+	 * (Note: Character.isLetter(char) tests if a char is an alphabetic letter.)
+	 *
 	 * countYZ("fez day") → 2
 	 * countYZ("day fez") → 2
 	 * countYZ("day fyyyz") → 2
-	 * 
+	 *
 	 */
 	public int countYZ(String str) {
 
@@ -272,7 +318,7 @@ public class Exec {
 				}
 			}
 		}
-		
+
 		return count;
 	}
 
