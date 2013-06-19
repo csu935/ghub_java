dca02f192aa9a009ce51d855037afffa5651376e
mars <mars@nmax.local>
2013-06-20 00:19:01 +0900

    isEverywhere

--- codingbat/com/array2/Exec.java
+++ codingbat/com/array2/Exec.java
@@ -12,6 +12,26 @@ public class Exec {
     DeOut.disp("------end output-------");
   }
   /*
+   * We'll say that a value is "everywhere" in an array
+   * if for every pair of adjacent elements in the array,
+   * at least one of the pair is that value.
+   * Return true if the given value is everywhere in the array.
+   *
+   * isEverywhere({1, 2, 1, 3}, 1) → true
+   * isEverywhere({1, 2, 1, 3}, 2) → false
+   * isEverywhere({1, 2, 1, 3, 4}, 1) → false
+   * isEverywhere({3}, 1) → true
+   * isEverywhere({}, 1) → true
+   */
+  public boolean isEverywhere(int[] nums, int val) {
+    if(nums.length<2) return true;
+    int valCount = 0;
+    for(int i=0;i<nums.length-1;i++){
+      if(nums[i] == val || nums[i+1] == val) valCount++;
+    }
+    return valCount==nums.length-1;
+  }
+  /*
    * Given an array of ints, return true
    * if it contains no 1's or it contains no 4's.
    *
