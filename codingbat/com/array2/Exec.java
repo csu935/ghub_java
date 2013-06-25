db56aad88a474518567319c5e88b3c3aad4bf0a5
marsbarmania <reg.marsbarmania@gmail.com>
2013-06-25 12:51:27 +0900

    pre4

--- codingbat/com/array2/Exec.java
+++ codingbat/com/array2/Exec.java
@@ -6,7 +6,34 @@ public class Exec {
   /**
    * @param args
    */
-  public static void main(String[] args) {
+  public static void main(String[] args) { }
+
+  /*
+   * Given a non-empty array of ints,
+   * return a new array containing the elements
+   * from the original array that come before the first 4 in the original array.
+   * The original array will contain at least one 4.
+   * Note that it is valid in java to create an array of length 0.
+   *
+   * pre4({1, 2, 4, 1}) → {1, 2}
+   * pre4({3, 1, 4}) → {3, 1}
+   * pre4({1, 4, 4}) → {1}
+   */
+  public int[] pre4(int[] nums) {
+    int n = 0;
+    while(n<nums.length-1){
+      //DeOut.disp(nums[n]+" len +> "+n);
+      if(nums[n]==4) break;
+      n++;
+    }
+    //DeOut.disp(len);
+    int[] front4 = new int[n];
+    if(n>0){
+      for(int i=0;i<n;i++){
+        front4[i] = nums[i];
+      }
+    }
+    return front4;
   }
 
   /*
