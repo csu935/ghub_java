54b8a4c247d15a4b8f92396ae3e86a3622bfd295
marsbarmania <reg.marsbarmania@gmail.com>
2013-06-25 11:41:58 +0900

    fizzArray3

--- codingbat/com/array2/Exec.java
+++ codingbat/com/array2/Exec.java
@@ -11,6 +11,29 @@ public class Exec {
     DeOut.disp("--------output---------");
     DeOut.disp("------end output-------");
   }
+
+  /*
+   * Given start and end numbers,
+   * return a new array containing the sequence of integers
+   * from start up to but not including end,
+   * so start=5 and end=10 yields {5, 6, 7, 8, 9}.
+   * The end number will be greater or equal to the start number.
+   * Note that a length-0 array is valid. (See also: FizzBuzz Code)
+   *
+   * fizzArray3(5, 10) → {5, 6, 7, 8, 9}
+   * fizzArray3(11, 18) → {11, 12, 13, 14, 15, 16, 17}
+   * fizzArray3(1, 3) → {1, 2}
+   */
+  public int[] fizzArray3(int start, int end) {
+      int len = end-start;
+      int[] arry = new int[len];
+      if(len==0) return arry;
+      for(int i=0;i<len;i++){
+        arry[i] = start + i;
+      }
+      return arry;
+  }
+
   /*
    * Return true if the array contains, somewhere,
    * three increasing adjacent numbers like .... 4, 5, 6, ... or 23, 24, 25.
