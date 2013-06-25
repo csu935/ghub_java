f4cbe82eeaee37790d06ac1ea2582d8399ff27ae
marsbarmania <reg.marsbarmania@gmail.com>
2013-06-25 11:51:18 +0900

    shiftLeft

--- codingbat/com/array2/Exec.java
+++ codingbat/com/array2/Exec.java
@@ -13,6 +13,26 @@ public class Exec {
   }
 
   /*
+   * Return an array that is "left shifted"
+   * by one -- so {6, 2, 5, 3} returns {2, 5, 3, 6}.
+   * You may modify and return the given array, or return a new array.
+   *
+   * shiftLeft({6, 2, 5, 3}) → {2, 5, 3, 6}
+   * shiftLeft({1, 2}) → {2, 1}
+   * shiftLeft({1}) → {1}
+   */
+  public int[] shiftLeft(int[] nums) {
+    if(nums.length<2) return nums;
+    int[] shifted = new int[nums.length];
+    for(int i=0;i<nums.length;i++){
+      if(i!=nums.length-1) shifted[i] = nums[i+1];
+      else shifted[i] = nums[0];
+
+    }
+    return shifted;
+  }
+
+  /*
    * Given start and end numbers,
    * return a new array containing the sequence of integers
    * from start up to but not including end,
