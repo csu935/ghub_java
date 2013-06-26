8ceb3a836735ff8f1a01de7e83dcf58b3186a5a9
marsbarmania <reg.marsbarmania@gmail.com>
2013-06-26 20:03:40 +0900

    notAlone

--- codingbat/com/array2/Exec.java
+++ codingbat/com/array2/Exec.java
@@ -7,7 +7,34 @@ public class Exec {
    * @param args
    */
   public static void main(String[] args) { }
-
+  
+  /*
+   * We'll say that an element in an array is "alone" 
+	 * if there are values before and after it, 
+	 * and those values are different from it. 
+	 * Return a version of the given array where every 
+	 * instance of the given value which is alone is replaced by 
+	 * whichever value to its left or right is larger. 
+	 * 
+	 * notAlone({1, 2, 3}, 2) → {1, 3, 3}
+	 * notAlone({1, 2, 3, 2, 5, 2}, 2) → {1, 3, 3, 5, 5, 2}
+	 * notAlone({3, 4}, 3) → {3, 4}
+	 * 
+	 * notAlone({1, 3, 1, 2}, 1) → {1, 3, 3, 2}
+	 * notAlone({7, 1, 6}, 1) → {7, 7, 6}
+	 * notAlone({1, 1, 1, 2}, 1) → {1, 1, 1, 2}
+	 */
+	public int[] notAlone(int[] nums, int val) {
+		if(nums.length<2) return nums;
+		for(int i=0;i<nums.length;i++){
+			if(nums[i]==val&&i>0&&i<nums.length-1){
+				if(nums[i]!=nums[i+1]&&nums[i]!=nums[i-1]){	
+					nums[i] = Math.max(nums[i-1], nums[i+1]);
+				}
+			}
+		}
+		return nums;
+	}
   /*
    * Given a non-empty array of ints, return a new array containing
    * the elements from the original array that come after the last 4
