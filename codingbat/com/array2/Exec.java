b7e03c84ea94505033d35c37dff71eb153cab7a7
mars <mars@nmax.local>
2013-06-28 18:59:49 +0900

    fizzBuzz

--- codingbat/com/array2/Exec.java
+++ codingbat/com/array2/Exec.java
@@ -9,6 +9,47 @@ public class Exec {
   public static void main(String[] args) { }
 
   /*
+   * This is slightly more difficult version of the famous FizzBuzz problem
+   * which is sometimes given as a first problem for job interviews.
+   * (See also: FizzBuzz Code.)
+   * Consider the series of numbers beginning at start
+   * and running up to but not including end,
+   * so for example start=1 and end=5 gives the series 1, 2, 3, 4.
+   * Return a new String[] array containing the string
+   * form of these numbers, except for multiples of 3,
+   * use "Fizz" instead of the number, for multiples of 5 use "Buzz",
+   * and for multiples of both 3 and 5 use "FizzBuzz". In Java,
+   * String.valueOf(xxx) will make the String form of an int or other type.
+   * This version is a little more complicated than the usual version
+   * since you have to allocate and index into an array instead of just printing,
+   * and we vary the start/end instead of just always doing 1..100.
+   *
+   * fizzBuzz(1, 6) → {"1", "2", "Fizz", "4", "Buzz"}
+   * fizzBuzz(1, 8) → {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7"}
+   * fizzBuzz(1, 11) → {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz"}
+   */
+  public String[] fizzBuzz(int start, int end) {
+      int len = end - start;
+      String[] arry = new String[len];
+      if(len <= 0) return arry;
+      for(int i=1; i<len+1;i++){
+        int tmp = i+start-1;
+        int fizz = tmp % 3;
+        int buzz = tmp % 5;
+        String s = "";
+        if(fizz==0){
+          if(buzz==0) s = "FizzBuzz";
+          else s = "Fizz";
+        }else{
+          if(buzz==0) s = "Buzz";
+          else s = String.valueOf(tmp);
+        }
+        arry[i-1] = s;
+      }
+      return arry;
+  }
+
+  /*
    * Return an array that contains the exact same numbers as the given array,
    * but rearranged so that all the even numbers come before all the odd numbers.
    * Other than that, the numbers can be in any order.
