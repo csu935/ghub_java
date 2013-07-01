93c4e1b58944dd1631e011ff38d139dc4bf1d4b3
marsbarmania <reg.marsbarmania@gmail.com>
2013-07-01 19:16:13 +0900

    fix45

--- codingbat/com/array3/Exec.java
+++ codingbat/com/array3/Exec.java
@@ -9,6 +9,46 @@ public class Exec {
   public static void main(String[] args) { }
 
   /*
+   * (This is a slightly harder version of the fix34 problem.)
+   * Return an array that contains exactly the same numbers
+   * as the given array, but rearranged so that every 4 is immediately
+   * followed by a 5. Do not move the 4's,
+   * but every other number may move.
+   * The array contains the same number of 4's and 5's,
+   * and every 4 has a number after it that is not a 4.
+   * In this version, 5's may appear anywhere in the original array.
+   *
+   * fix45({5, 4, 9, 4, 9, 5}) → {9, 4, 5, 4, 5, 9}
+   * fix45({1, 4, 1, 5}) → {1, 4, 5, 1}
+   * fix45({1, 4, 1, 5, 5, 4, 1}) → {1, 4, 5, 1, 1, 4, 5}
+   *
+   * fix45({5, 5, 4, 1, 4, 1}) → {1, 1, 4, 5, 4, 5}
+   * fix45({4, 5, 4, 1, 5}) → {4, 5, 4, 5, 1}
+   */
+  public int[] fix45(int[] nums) {
+
+    for(int i=0;i<nums.length;i++){
+      if(nums[i]==4&&nums[i+1]!=5){
+
+        int t = nums[i+1];
+        int k =0;
+        while(k<nums.length){
+
+          if((k==0&&nums[k]==5)||
+              (k>0&&nums[k]==5&&nums[k-1]!=4)){
+            nums[k]=t;
+            nums[i+1]=5;
+            break;
+          }
+
+          k++;
+        }
+      }
+    }
+    return nums;
+  }
+
+  /*
    * Return an array that contains exactly the same numbers
    * as the given array, but rearranged so that every 3
    * is immediately followed by a 4. Do not move the 3's,
