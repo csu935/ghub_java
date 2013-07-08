c628817d29181d3f9a2fc918bd765b55c4baaaa6
marsbarmania <reg.marsbarmania@gmail.com>
2013-07-08 18:00:17 +0900

    squareUp

--- codingbat/com/array3/Exec.java
+++ codingbat/com/array3/Exec.java
@@ -99,6 +99,38 @@ public class Exec {
   }
 
   /*
+   * Given n>=0, create an array length n*n with the following pattern,
+   * shown here for n=3 : {0, 0, 1,    0, 2, 1,    3, 2, 1}
+   * (spaces added to show the 3 groups).
+   *
+   * squareUp(3) → {0, 0, 1, 0, 2, 1, 3, 2, 1}
+   * squareUp(2) → {0, 1, 2, 1}
+   * squareUp(4) → {0, 0, 0, 1, 0, 0, 2, 1, 0, 3, 2, 1, 4, 3, 2, 1}
+   */
+  public int[] squareUp(int n) {
+    int[] nums = new int[n*n];
+    int count = 1;
+    int k = 0;
+    boolean start = false;
+
+    for(int i=0;i<n;i++){
+
+      for(int j=0;j<n;j++){
+
+        if(j%n==n-count) start = true;
+
+        if(start) nums[k]= n - (k % n);
+        else nums[k] = 0;
+        k++;
+      }
+      start = false;
+      count++;
+    }
+
+    return nums;
+  }
+
+  /*
    * Given two arrays of ints sorted in increasing order,
    * outer and inner, return true if all of the numbers
    * in inner appear in outer. The best solution makes only a single "linear"
