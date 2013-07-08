6ac827cbd76032993c3ad81a49e79ba0dbee9224
marsbarmania <reg.marsbarmania@gmail.com>
2013-07-08 19:06:17 +0900

    seriesUp

--- codingbat/com/array3/Exec.java
+++ codingbat/com/array3/Exec.java
@@ -99,6 +99,35 @@ public class Exec {
   }
 
   /*
+   * Given n>=0, create an array with the pattern
+   * {1,    1, 2,    1, 2, 3,   ... 1, 2, 3 .. n}
+   *  (spaces added to show the grouping).
+   *  Note that the length of the array will be 1 + 2 + 3 ... + n,
+   *  which is known to sum to exactly n*(n + 1)/2.
+   *
+   *  seriesUp(3) → {1, 1, 2, 1, 2, 3}
+   *  seriesUp(4) → {1, 1, 2, 1, 2, 3, 1, 2, 3, 4}
+   *  seriesUp(2) → {1, 1, 2}
+   */
+  public int[] seriesUp(int n) {
+    int[] nums= new int[n*(n + 1)/2];
+    int loopCnt = 0;
+    int k = 0;
+
+    for(int i=0;i<n;i++){
+      // set loop count in every loops
+      loopCnt = i + 1;
+      for(int j=0;j<loopCnt;j++){
+        // j + 1 is the value
+        nums[k] = j+1;
+        k++;
+      }
+      loopCnt++;
+    }
+    return nums;
+  }
+
+  /*
    * Given n>=0, create an array length n*n with the following pattern,
    * shown here for n=3 : {0, 0, 1,    0, 2, 1,    3, 2, 1}
    * (spaces added to show the 3 groups).
