a79a0270d33ef610712a3becd80823375e62711a
marsbarmania <reg.marsbarmania@gmail.com>
2013-07-09 18:31:37 +0900

    scoresAverage & average

--- codingbat/com/AP1/Exam.java
+++ codingbat/com/AP1/Exam.java
@@ -6,6 +6,49 @@ public class Exam {
   public static void main(String[] args) { }
 
   /*
+   * Given an array of scores, compute the int average of the first half
+   * and the second half, and return whichever is larger.
+   * We'll say that the second half begins at index length/2.
+   * The array length will be at least 2. To practice decomposition,
+   * write a separate helper method <br>int average(int[] scores,
+   * int start, int end) { which computes the average of the elements
+   * between indexes start..end. Call your helper method twice
+   * to implement scoresAverage().
+   * Write your helper method after your scoresAverage() method
+   * in the JavaBat text area. Normally you would compute averages with doubles,
+   * but here we use ints so the expected results are exact.
+   *
+   * scoresAverage({2, 2, 4, 4}) → 4
+   * scoresAverage({4, 4, 4, 2, 2, 2}) → 4
+   * scoresAverage({3, 4, 5, 1, 2, 3}) → 4
+   *
+   * scoresAverage({5, 6}) → 6
+   * scoresAverage({5, 4}) → 5
+   */
+  public int scoresAverage(int[] scores) {
+    int len = scores.length;
+    if(len==2){
+      return Math.max(scores[0], scores[len-1]);
+    }else{
+      int first = average(scores,0,len/2);
+      int second = average(scores,len/2,len-1);
+      return Math.max(first,second);
+    }
+
+  }
+  /*
+   * int average(int[] scores, int start, int end) {
+   * which computes the average of the elements between indexes start..end.
+   */
+  int average(int[] scores, int start, int end) {
+    int sum = 0;
+    for(int i=0;i<end-start;i++){
+      sum += scores[i+start];
+    }
+    return sum/(end-start);
+  }
+
+  /*
    * Given an array of scores sorted in increasing order,
    * return true if the array contains 3 adjacent scores that
    * differ from each other by at most 2,
