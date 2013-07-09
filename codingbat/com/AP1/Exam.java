de889cfebfa05b97cb14bde403f8419df61fe3af
marsbarmania <reg.marsbarmania@gmail.com>
2013-07-09 15:45:14 +0900

    AP-1 scoresClump

--- codingbat/com/AP1/Exam.java
+++ codingbat/com/AP1/Exam.java
@@ -6,6 +6,29 @@ public class Exam {
   public static void main(String[] args) { }
 
   /*
+   * Given an array of scores sorted in increasing order,
+   * return true if the array contains 3 adjacent scores that
+   * differ from each other by at most 2,
+   * such as with {3, 4, 5} or {3, 5, 5}.
+   *
+   * scoresClump({3, 4, 5}) → true
+   * scoresClump({3, 4, 6}) → false
+   * scoresClump({1, 3, 5, 5}) → true
+   */
+  public boolean scoresClump(int[] scores) {
+    if(scores.length<3) return false;
+    for(int i=0;i<scores.length-2;i++){
+      if(scores[i+1]-scores[i]<=2 &&
+          scores[i+2]-scores[i]<=2 &&
+          scores[i+2]-scores[i+1]<=2){
+
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /*
    * Given an array of scores, return true
    * if there are scores of 100 next to each other in the array.
    * The array length will be at least 2.
