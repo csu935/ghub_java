da47395bd899fdef4d7eddb53185f30e2b3f674c
marsbarmania <reg.marsbarmania@gmail.com>
2013-07-09 12:47:33 +0900

    added new package 'AP1'

---
+++ codingbat/com/AP1/Exam.java
@@ -0,0 +1,20 @@
+package codingbat.com.AP1;
+import lib.DeOut;
+
+public class Exam {
+
+  public static void main(String[] args) { }
+
+  /*
+   * Given an array of scores, return true if each score is equal
+   * or greater than the one before. The array will be length 2 or more.
+   *
+   * scoresIncreasing({1, 3, 4}) → true
+   * scoresIncreasing({1, 3, 2}) → false
+   * scoresIncreasing({1, 1, 4}) → true
+   */
+  public boolean scoresIncreasing(int[] scores) {
+    return true;
+  }
+
+}
