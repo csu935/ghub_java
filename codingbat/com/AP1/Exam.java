0d4c4fd2cf250587df2f8dd428c45f7d4ff51920
marsbarmania <reg.marsbarmania@gmail.com>
2013-07-10 11:36:32 +0900

    AP-1: wordsCount

--- codingbat/com/AP1/Exam.java
+++ codingbat/com/AP1/Exam.java
@@ -6,6 +6,22 @@ public class Exam {
   public static void main(String[] args) { }
 
   /*
+   * Given an array of strings, return the count of the number
+   * of strings with the given length.
+   *
+   * wordsCount({"a", "bb", "b", "ccc"}, 1) → 2
+   * wordsCount({"a", "bb", "b", "ccc"}, 3) → 1
+   * wordsCount({"a", "bb", "b", "ccc"}, 4) → 0
+   */
+  public int wordsCount(String[] words, int len) {
+    int count = 0;
+    for(int i=0;i<words.length;i++){
+      if(words[i].length()==len) count++;
+    }
+    return count;
+  }
+
+  /*
    * Given an array of scores, compute the int average of the first half
    * and the second half, and return whichever is larger.
    * We'll say that the second half begins at index length/2.
