79fdd2803041eb733671a2b3ea600ab9e4995687
marsbarmania <reg.marsbarmania@gmail.com>
2013-07-10 11:42:59 +0900

    AP-1: wordsFront

--- codingbat/com/AP1/Exam.java
+++ codingbat/com/AP1/Exam.java
@@ -6,6 +6,22 @@ public class Exam {
   public static void main(String[] args) { }
 
   /*
+   * Given an array of strings, return a new array containing
+   * the first N strings. N will be in the range 1..length.
+   *
+   * wordsFront({"a", "b", "c", "d"}, 1) → {"a"}
+   * wordsFront({"a", "b", "c", "d"}, 2) → {"a", "b"}
+   * wordsFront({"a", "b", "c", "d"}, 3) → {"a", "b", "c"}
+   */
+  public String[] wordsFront(String[] words, int n) {
+    String[] ans = new String[n];
+    for(int i=0;i<n;i++){
+      ans[i] = words[i];
+    }
+    return ans;
+  }
+
+  /*
    * Given an array of strings, return the count of the number
    * of strings with the given length.
    *
