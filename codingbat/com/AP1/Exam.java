3db1714aec0220b8366f11d4246f93f62f8b1a15
mars <mars@nmax.local>
2013-07-11 20:05:40 +0900

    copyEndy & isEndy

--- codingbat/com/AP1/Exam.java
+++ codingbat/com/AP1/Exam.java
@@ -8,6 +8,45 @@ public class Exam {
   public static void main(String[] args) { }
 
   /*
+   * We'll say that a positive int n is "endy"
+   * if it is in the range 0..10 or 90..100 (inclusive).
+   * Given an array of positive ints, return a new array of
+   * length "count" containing the first endy numbers from the original array.
+   * Decompose out a separate isEndy(int n) method to test
+   * if a number is endy. The original array will contain
+   * at least "count" endy numbers.
+   *
+   * copyEndy({9, 11, 90, 22, 6}, 2) → {9, 90}
+   * copyEndy({9, 11, 90, 22, 6}, 3) → {9, 90, 6}
+   * copyEndy({12, 1, 1, 13, 0, 20}, 2) → {1, 1}
+   */
+  public int[] copyEndy(int[] nums, int count) {
+    int[] endy = new int[count];
+    int n = 0;
+    for(int i=0;i<nums.length;i++){
+      if(isEndy(nums[i])){
+        if(n<count){
+          endy[n] = nums[i];
+          n++;
+        }else{
+          break;
+        }
+      }
+    }
+    return endy;
+  }
+  /*
+   * isEndy(int n) method
+   */
+  private boolean isEndy(int n){
+    if((n>=0 && 10>=n) ||
+        n>=90 && 100>=n){
+      return true;
+    }
+    return false;
+  }
+
+  /*
    * Given an array of positive ints, return a new array
    * of length "count" containing the first even numbers
    * from the original array. The original array will contain
