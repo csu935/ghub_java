78e626c6f1301ef0491af790aba55d2be0efd8ba
marsbarmania <reg.marsbarmania@gmail.com>
2013-07-12 13:15:23 +0900

    scoresSpecial & helper method

--- codingbat/com/AP1/Exam.java
+++ codingbat/com/AP1/Exam.java
@@ -8,6 +8,32 @@ public class Exam {
   public static void main(String[] args) { }
 
   /*
+   * Given two arrays, A and B, of non-negative int scores.
+   * A "special" score is one which is a multiple of 10, such as 40 or 90.
+   * Return the sum of largest special score in A and the largest special score in B.
+   * To practice decomposition, write a separate helper method
+   * which finds the largest special score in an array.
+   * Write your helper method after your scoresSpecial() method in the JavaBat text area.
+   *
+   * scoresSpecial({12, 10, 4}, {2, 20, 30}) → 40
+   * scoresSpecial({20, 10, 4}, {2, 20, 10}) → 40
+   * scoresSpecial({12, 11, 4}, {2, 20, 31}) → 20
+   */
+  public int scoresSpecial(int[] a, int[] b) {
+    return findMaxSpecialScore(a)+findMaxSpecialScore(b);
+  }
+   /* helper method */
+  private int findMaxSpecialScore(int[] nums){
+    int max = 0;
+    for(int i=0;i<nums.length;i++){
+      if(nums[i]%10==0){
+        max = Math.max(max, nums[i]);
+      }
+    }
+    return max;
+  }
+
+  /*
    * Given an array of strings, return a new array without the strings
    * that are equal to the target string.
    * One approach is to count the occurrences of the target string,
