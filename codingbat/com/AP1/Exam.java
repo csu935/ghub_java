fbc454f63173e19e2dcfea078bd7b37380ea0a2d
marsbarmania <reg.marsbarmania@gmail.com>
2013-07-12 12:58:17 +0900

    wordsWithout

--- codingbat/com/AP1/Exam.java
+++ codingbat/com/AP1/Exam.java
@@ -8,6 +8,32 @@ public class Exam {
   public static void main(String[] args) { }
 
   /*
+   * Given an array of strings, return a new array without the strings
+   * that are equal to the target string.
+   * One approach is to count the occurrences of the target string,
+   * make a new array of the correct length, and then copy over the correct strings.
+   *
+   * wordsWithout({"a", "b", "c", "a"}, "a") → {"b", "c"}
+   * wordsWithout({"a", "b", "c", "a"}, "b") → {"a", "c", "a"}
+   * wordsWithout({"a", "b", "c", "a"}, "c") → {"a", "b", "a"}
+   */
+  public String[] wordsWithout(String[] words, String target) {
+    int len = 0;
+    for(int i=0;i<words.length;i++){
+      if(!words[i].equals(target)) len++;
+    }
+    String[] without = new String[len];
+    int start = 0;
+    for(int i=0;i<words.length;i++){
+      if(!words[i].equals(target)){
+        without[start] = words[i];
+        start++;
+      }
+    }
+    return without;
+  }
+
+  /*
    * The "key" array is an array containing the correct answers to an exam,
    * like {"a", "a", "b", "b"}. the "answers" array contains a student's answers,
    * with "?" representing a question left blank.
