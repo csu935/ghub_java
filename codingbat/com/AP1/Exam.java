0e52e94436a49c46d2497435d5640762f7f4df2e
marsbarmania <reg.marsbarmania@gmail.com>
2013-07-12 12:25:45 +0900

    scoreUp

--- codingbat/com/AP1/Exam.java
+++ codingbat/com/AP1/Exam.java
@@ -8,6 +8,32 @@ public class Exam {
   public static void main(String[] args) { }
 
   /*
+   * The "key" array is an array containing the correct answers to an exam,
+   * like {"a", "a", "b", "b"}. the "answers" array contains a student's answers,
+   * with "?" representing a question left blank.
+   * The two arrays are not empty and are the same length.
+   * Return the score for this array of answers, giving +4 for each correct answer, -1
+   * for each incorrect answer, and +0 for each blank answer.
+   *
+   * scoreUp({"a", "a", "b", "b"}, {"a", "c", "b", "c"}) → 6
+   * scoreUp({"a", "a", "b", "b"}, {"a", "a", "b", "c"}) → 11
+   * scoreUp({"a", "a", "b", "b"}, {"a", "a", "b", "b"}) → 16
+   */
+  public int scoreUp(String[] key, String[] answers) {
+    int score = 0;
+    for(int i=0;i<key.length;i++){
+      if(key[i].equals(answers[i])){
+        score += 4;
+      }else if(answers[i].equals("?")){
+        score += 0;
+      }else{
+        score -= 1;
+      }
+    }
+    return score;
+  }
+
+  /*
    * Given 2 arrays that are the same length containing strings,
    * compare the 1st string in one array to the 1st string in the other array,
    * the 2nd to the 2nd and so on. Count the number of times
