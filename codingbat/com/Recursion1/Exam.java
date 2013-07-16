32878b59dd66cb89aa5aecd67f443367e8e24b6a
marsbarmania <marsbar@marsmac.local>
2013-07-16 22:57:14 +0900

    /*

--- codingbat/com/Recursion1/Exam.java
+++ codingbat/com/Recursion1/Exam.java
@@ -7,6 +7,25 @@ public class Exam {
   public static void main(String[] args) { Exam ex = new Exam(); }
 
   /*
+   * The fibonacci sequence is a famous bit of mathematics,
+   * and it happens to have a recursive definition.
+   * The first two values in the sequence are 0 and 1 (essentially 2 base cases).
+   * Each subsequent value is the sum of the previous two values,
+   * so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on.
+   * Define a recursive fibonacci(n) method that returns the nth fibonacci number,
+   *  with n=0 representing the start of the sequence.
+   *
+   *  fibonacci(0) → 0
+   *  fibonacci(1) → 1
+   *  fibonacci(2) → 1
+   */
+  public int fibonacci(int n) {
+    if(n==0) return 0;
+    if(n==1) return 1;
+    return fibonacci(n-1)+fibonacci(n-2);
+  }
+
+  /*
    * We have a number of bunnies and each bunny has two big floppy ears.
    * We want to compute the total number of ears across all the bunnies recursively
    * (without loops or multiplication).
