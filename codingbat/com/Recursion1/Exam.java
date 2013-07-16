e8a4d49e2c3ad6818f1dfdd08431a3a84adba330
marsbarmania <reg.marsbarmania@gmail.com>
2013-07-16 20:09:20 +0900

    factorial

--- codingbat/com/Recursion1/Exam.java
+++ codingbat/com/Recursion1/Exam.java
@@ -6,5 +6,19 @@ public class Exam {
 
   public static void main(String[] args) { Exam ex = new Exam(); }
 
+  /*
+   * Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1.
+   * Compute the result recursively (without loops).
+   *
+   * factorial(1) → 1
+   * factorial(2) → 2
+   * factorial(3) → 6
+   */
+  public int factorial(int n) {
 
+    // less than 1 inclusive is always 1
+    if(n<=1) return 1;
+
+    return n * factorial(n-1);
+  }
 }
