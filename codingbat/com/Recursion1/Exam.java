e1619f2d47b5f26a1ca208cb063f3f7f7d2c8e6f
marsbarmania <reg.marsbarmania@gmail.com>
2013-07-16 20:13:07 +0900

    added hint for factorial

--- codingbat/com/Recursion1/Exam.java
+++ codingbat/com/Recursion1/Exam.java
@@ -15,10 +15,14 @@ public class Exam {
    * factorial(3) → 6
    */
   public int factorial(int n) {
-
+    // Base case: if n is 1, we can return the answer directly
     // less than 1 inclusive is always 1
     if(n<=1) return 1;
 
+    // Recursion: otherwise make a recursive call with n-1
+    // (towards the base case), i.e. call factorial(n-1).
+    // Assume the recursive call works correctly, and fix up
+    // what it returns to make our result.
     return n * factorial(n-1);
   }
 }
