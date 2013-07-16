7cc5c1512ea72bc4d475c4a5d4f194bce50e7b57
marsbarmania <reg.marsbarmania@gmail.com>
2013-07-16 20:32:28 +0900

    bunnyEars

--- codingbat/com/Recursion1/Exam.java
+++ codingbat/com/Recursion1/Exam.java
@@ -7,6 +7,25 @@ public class Exam {
   public static void main(String[] args) { Exam ex = new Exam(); }
 
   /*
+   * We have a number of bunnies and each bunny has two big floppy ears.
+   * We want to compute the total number of ears across all the bunnies recursively
+   * (without loops or multiplication).
+   *
+   * bunnyEars(0) → 0
+   * bunnyEars(1) → 2
+   * bunnyEars(2) → 4
+   *
+   * Hint: First detect the base case (bunnies == 0), and in that case just return 0. Otherwise,
+   * make a recursive call to bunnyEars(bunnies-1).
+   * Trust that the recursive call returns the correct value, and fix it up by adding 2.
+   */
+  public int bunnyEars(int bunnies) {
+    // Base case: if bunnies==0, just return 0.
+    if (bunnies == 0) return 0;
+    return 2 + bunnyEars(bunnies-1);
+  }
+
+  /*
    * Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1.
    * Compute the result recursively (without loops).
    *
