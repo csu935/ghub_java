08723a64deca1aecf4cce6643c2c1fe550cfce35
marsbarmania <reg.marsbarmania@gmail.com>
2013-07-17 19:15:19 +0900

    triangle

--- codingbat/com/Recursion1/Exam.java
+++ codingbat/com/Recursion1/Exam.java
@@ -7,6 +7,22 @@ public class Exam {
   public static void main(String[] args) { Exam ex = new Exam(); }
 
   /*
+   * We have triangle made of blocks. The topmost row has 1 block,
+   * the next row down has 2 blocks, the next row has 3 blocks,
+   * and so on. Compute recursively (no loops or multiplication)
+   * the total number of blocks in such a triangle
+   * with the given number of rows.
+   *
+   * triangle(0) → 0
+   * triangle(1) → 1
+   * triangle(2) → 3
+   */
+  public int triangle(int rows) {
+    if(rows==0) return 0;
+    return rows + triangle(rows-1);
+  }
+
+  /*
    * We have bunnies standing in a line, numbered 1, 2, ...
    * The odd bunnies (1, 3, ..) have the normal 2 ears.
    * The even bunnies (2, 4, ..) we'll say have 3 ears,
