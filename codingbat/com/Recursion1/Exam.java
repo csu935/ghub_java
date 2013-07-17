17d0a107dddfb881c963a8d065cc005a3a67512b
marsbarmania <reg.marsbarmania@gmail.com>
2013-07-17 20:29:02 +0900

    sumDigits

--- codingbat/com/Recursion1/Exam.java
+++ codingbat/com/Recursion1/Exam.java
@@ -7,6 +7,20 @@ public class Exam {
   public static void main(String[] args) { Exam ex = new Exam(); }
 
   /*
+   * Given a non-negative int n,
+   * return the sum of its digits recursively (no loops).
+   * Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6),
+   * while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+   *
+   * sumDigits(126) → 9
+   * sumDigits(49) → 13
+   * sumDigits(12) → 3
+   */
+  public int sumDigits(int n) {
+    return (n/10==0)? n : (n % 10) + sumDigits(n/10);
+  }
+
+  /*
    * We have triangle made of blocks. The topmost row has 1 block,
    * the next row down has 2 blocks, the next row has 3 blocks,
    * and so on. Compute recursively (no loops or multiplication)
@@ -20,6 +34,9 @@ public class Exam {
   public int triangle(int rows) {
     if(rows==0) return 0;
     return rows + triangle(rows-1);
+
+    // Solution notes: what is triangle(20)? It's whatever
+    // triangle(19) returns, plus 20.
   }
 
   /*
