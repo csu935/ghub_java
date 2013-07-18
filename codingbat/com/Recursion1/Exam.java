600080913d3cda61c37ee69cc84e36488744216d
marsbarmania <reg.marsbarmania@gmail.com>
2013-07-18 18:14:25 +0900

    count7

--- codingbat/com/Recursion1/Exam.java
+++ codingbat/com/Recursion1/Exam.java
@@ -7,6 +7,29 @@ public class Exam {
   public static void main(String[] args) { Exam ex = new Exam(); }
 
   /*
+   * Given a non-negative int n, return the count of the occurrences of 7 as a digit,
+   * so for example 717 yields 2. (no loops).
+   * Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6),
+   * while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+   *
+   * count7(717) → 2
+   * count7(7) → 1
+   * count7(123) → 0
+   */
+  public int count7(int n) {
+    int count = 0;
+    if(n/10==0){
+      // rightmost is '7'
+      if(n==7) count += 1;
+    }else{
+      // recursively
+      if(n % 10 == 7) count += 1 + count7(n/10);
+      else  count += count7(n/10);
+    }
+    return count;
+  }
+
+  /*
    * Given a non-negative int n,
    * return the sum of its digits recursively (no loops).
    * Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6),
