3f2acc752541fa540b305ba4017d3092880739d5
marsbarmania <reg.marsbarmania@gmail.com>
2013-07-19 19:05:01 +0900

    changePi

--- codingbat/com/Recursion1/Exam.java
+++ codingbat/com/Recursion1/Exam.java
@@ -7,6 +7,29 @@ public class Exam {
   public static void main(String[] args) { Exam ex = new Exam(); }
 
   /*
+   * Given a string, compute recursively (no loops)
+   * a new string where all appearances of "pi" have been
+   * replaced by "3.14".
+   *
+   * changePi("xpix") → "x3.14x"
+   * changePi("pipi") → "3.143.14"
+   * changePi("pip") → "3.14p"
+   */
+  public String changePi(String str) {
+    if(str.length()<2) return str;
+    String replaced = "";
+    int index = 1;
+    if(str.substring(0,2).equals("pi")) {
+      replaced = "3.14";
+      // 2 index skip
+      index = 2;
+    }else {
+      replaced = str.substring(0,1);
+    }
+    return replaced + changePi(str.substring(index));
+  }
+
+  /*
    * Given a string, compute recursively (no loops) a new string
    * where all the lowercase 'x' chars have been changed to 'y' chars.
    *
