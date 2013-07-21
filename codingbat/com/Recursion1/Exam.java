4fd02da3b1e9b587a6ed971405c798b733ad70b8
marsbarmania <marsbar@marsmac.local>
2013-07-21 23:38:20 +0900

    array6

--- codingbat/com/Recursion1/Exam.java
+++ codingbat/com/Recursion1/Exam.java
@@ -7,6 +7,30 @@ public class Exam {
   public static void main(String[] args) { Exam ex = new Exam(); }
 
   /*
+   * Given an array of ints, compute recursively if the array contains a 6.
+   * We'll use the convention of considering only the part of the array
+   * that begins at the given index. In this way,
+   * a recursive call can pass index+1 to move down the array.
+   * The initial call will pass in index as 0.
+   *
+   * array6({1, 6, 4}, 0) → true
+   * array6({1, 4}, 0) → false
+   * array6({6}, 0) → true
+   */
+  public boolean array6(int[] nums, int index) {
+    if(index<nums.length){
+      if(nums[index]==6){
+        return true;
+      }else{
+        index++;
+        return array6(nums,index);
+      }
+    }else{
+      return false;
+    }
+  }
+
+  /*
    * Given a string, compute recursively a new string
    * where all the 'x' chars have been removed.
    *
