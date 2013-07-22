083a16a0b9edd22875ad27cf96038ba9201fc803
marsbarmania <reg.marsbarmania@gmail.com>
2013-07-22 11:20:11 +0900

    array11

--- codingbat/com/Recursion1/Exam.java
+++ codingbat/com/Recursion1/Exam.java
@@ -7,6 +7,30 @@ public class Exam {
   public static void main(String[] args) { Exam ex = new Exam(); }
 
   /*
+   * Given an array of ints, compute recursively the number
+   * of times that the value 11 appears in the array.
+   * We'll use the convention of considering only the part
+   * of the array that begins at the given index. In this way,
+   * a recursive call can pass index+1 to move down the array.
+   * The initial call will pass in index as 0.
+   *
+   * array11({1, 2, 11}, 0) → 1
+   * array11({11, 11}, 0) → 2
+   * array11({1, 2, 3, 4}, 0) → 0
+   */
+  public int array11(int[] nums, int index) {
+    int count = 0;
+    if(index<nums.length){
+      if(nums[index]==11) count++;
+      index++;
+      // Return count + the result of array11
+      return count + array11(nums,index);
+    }else{
+      return count;
+    }
+  }
+
+  /*
    * Given an array of ints, compute recursively if the array contains a 6.
    * We'll use the convention of considering only the part of the array
    * that begins at the given index. In this way,
