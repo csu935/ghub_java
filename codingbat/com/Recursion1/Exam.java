3fbfcf44cbe8b34cc86a4feaacef9013f109a26f
marsbarmania <reg.marsbarmania@gmail.com>
2013-07-22 17:49:31 +0900

    countPairs

--- codingbat/com/Recursion1/Exam.java
+++ codingbat/com/Recursion1/Exam.java
@@ -7,6 +7,29 @@ public class Exam {
   public static void main(String[] args) { Exam ex = new Exam(); }
 
   /*
+   * We'll say that a "pair" in a string is two instances of a char separated by a char.
+   * So "AxA" the A's make a pair. Pair's can overlap,
+   * so "AxAxA" contains 3 pairs -- 2 for A and 1 for x.
+   * Recursively compute the number of pairs in the given string.
+   *
+   * countPairs("axa") → 1
+   * countPairs("axax") → 2
+   * countPairs("axbx") → 1
+   */
+  public int countPairs(String str) {
+    int count = 0;
+    int index = 0;
+    if(index<str.length()-2){
+      if(str.substring(index, index+1).equals(str.substring(index+2, index+3))){
+        count++;
+      }
+      return count + countPairs(str.substring(1));
+    }else{
+      return count;
+    }
+  }
+
+  /*
    * Given a string, compute recursively a new string
    * where all the lowercase 'x' chars have been moved to the end of
    * the string.
