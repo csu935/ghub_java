b9eceebe2607003eac24f288fc5c99083a81d16b
marsbarmania <marsbar@marsmac.local>
2013-08-16 19:37:59 +0900

    strCopies

--- codingbat/com/Recursion1/Exam.java
+++ codingbat/com/Recursion1/Exam.java
@@ -8,6 +8,45 @@ public class Exam {
 
   /*
    * Given a string and a non-empty substring sub,
+   * compute recursively if at least n copies of sub appear
+   * in the string somewhere, possibly with overlapping.
+   * N will be non-negative.
+   *
+   * strCopies("catcowcat", "cat", 2) → true
+   * strCopies("catcowcat", "cow", 2) → false
+   * strCopies("catcowcat", "cow", 1) → true
+   * strCopies("iiijjj", "x", 0) → true
+   */
+  public boolean strCopies(String str, String sub, int n) {
+    if (n == 0) return true;
+    int len = sub.length();
+    if (str.length() < len) return false;
+
+    if (str.substring(0, len).equals(sub)) {
+      // Found it, so subtract 1 from n in the recursion
+      return strCopies(str.substring(1), sub, n-1);
+    } else {
+      return strCopies(str.substring(1), sub, n);
+    }
+  }
+
+  /*
+  public boolean strCopies(String str, String sub, int n) {
+    if(n==0) return true;
+    if(str.substring(0,sub.length()).equals(sub)) {
+      n--;
+      if(n==0) return true;
+    }
+
+    if(str.substring(1).length() >= sub.length()) {
+      return strCopies(str.substring(1),sub,n);
+    }
+    return false;
+  }
+  */
+
+  /*
+   * Given a string and a non-empty substring sub,
    * compute recursively the number of times that
    * sub appears in the string, without the sub strings overlapping.
    *
