d7507c67af1cfa8d3a2fceaa7bc17f0bbb8ac569
marsbarmania <reg.marsbarmania@gmail.com>
2013-08-20 20:39:15 +0900

    groupSum5

--- codingbat/com/Recursion2/Exam.java
+++ codingbat/com/Recursion2/Exam.java
@@ -4,6 +4,39 @@ import lib.DeOut;
 public class Exam {
 
   public static void main(String[] args) { }
+
+  /*
+   * Given an array of ints, is it possible to choose a group of some of the ints,
+   * such that the group sums to the given target with these additional constraints:
+   * all multiples of 5 in the array must be included in the group.
+   * If the value immediately following a multiple of 5 is 1,
+   * it must not be chosen. (No loops needed.)
+   *
+   * groupSum5(0, {2, 5, 10, 4}, 19) → true
+   * groupSum5(0, {2, 5, 10, 4}, 17) → true
+   * groupSum5(0, {2, 5, 10, 4}, 12) → false
+   * groupSum5(0, {3, 5, 1}, 9) → false
+   * groupSum5(0, {2, 5, 4, 10}, 12) → false
+   * groupSum5(0, {3, 5, 1}, 5) → true
+   * groupSum5(0, {1, 3, 5}, 5) → true
+   * groupSum5(0, {1}, 1) → true
+   */
+  public boolean groupSum5(int start, int[] nums, int target) {
+    if (start >= nums.length) return (target == 0);
+    // nums[start]が5の倍数の場合
+    if (nums[start] % 5 == 0) {
+      // ただし、次の数が1の場合は無視する処理
+       if (start < nums.length - 1 && nums[start + 1] == 1){
+        // インデックスは2つ進める
+         return groupSum5(start + 2, nums, target - nums[start]);
+       }
+       //　この処理はデフォルト
+      return groupSum5(start + 1, nums, target- nums[start]);
+    }
+    return groupSum5(start + 1, nums, target - nums[start])
+              || groupSum5(start + 1, nums, target);
+  }
+
   /*
    * Given an array of ints, is it possible to choose a group of some of the ints,
    * such that the group sums to the given target with this additional constraint:
