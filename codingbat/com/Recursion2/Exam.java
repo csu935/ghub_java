19ba3a333f49417cf10c48ce5fc96d1bb532e15b
marsbarmania <reg.marsbarmania@gmail.com>
2013-08-20 15:44:04 +0900

    groupNoAdj

--- codingbat/com/Recursion2/Exam.java
+++ codingbat/com/Recursion2/Exam.java
@@ -4,6 +4,27 @@ import lib.DeOut;
 public class Exam {
 
   public static void main(String[] args) { }
+  /*
+   * Given an array of ints, is it possible to choose a group of some of the ints,
+   * such that the group sums to the given target with this additional constraint:
+   * If a value in the array is chosen to be in the group,
+   * the value immediately following it in the array must not be chosen.
+   * (No loops needed.)
+   *
+   * groupNoAdj(0, {2, 5, 10, 4}, 12) → true
+   * groupNoAdj(0, {2, 5, 10, 4}, 14) → false
+   * groupNoAdj(0, {2, 5, 10, 4}, 7) → false
+   */
+  public boolean groupNoAdj(int start, int[] nums, int target) {
+    // target==0になっているなら、true
+    if (start >= nums.length) return (target == 0);
+    // startが選ばれていない場合は、targetにはなにもせず次の値にする
+    if (groupNoAdj(start+1, nums, target)) return true;
+    // nums[start]が選ばれている時、2つ飛ばす
+    if (groupNoAdj(start+2, nums, target-nums[start])) return true;
+
+    return false;
+  }
 
   /*
    * Given an array of ints, is it possible to choose a group of some of the ints,
@@ -23,7 +44,7 @@ public class Exam {
    *
    */
   public boolean groupSum6(int start, int[] nums, int target) {
-    // target==0であれば、true
+    // target==0になっているなら、true
     if (start >= nums.length) return (target == 0);
     // nums[start]==6の場合、target-6して再帰処理
     if (nums[start]==6) return groupSum6(start+1, nums, target-6);
